f64d7aad5707715f141c50905654233d70c5a783
Scott aka: Keldo <scilley@outlook.com>
2020-08-19 11:34:26 -0400

    Update header image

--- front-page.php
+++ front-page.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
   <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/img/header.jpg') ?>);"></div>
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/img/library-hero.jpg') ?>);"></div>
     <div class="page-banner__content container t-center c-white">
       <h1 class="headline headline--large">Welcome!</h1>
       <h2 class="headline headline--medium"><?php echo get_bloginfo( 'name' ); ?>"</h2>
