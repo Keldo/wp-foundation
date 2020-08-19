111ad7fa998a2a6114a307cff6001523b7b1c993
Keldo <scilley@outlook.com>
2020-08-19 12:09:25 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,21 +1,21 @@
 <?php
 
-function dw-foundation_files() {
+function dw_foundation_files() {
   wp_enqueue_script('main-brokenfence-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
   wp_enqueue_style('brokenfence_main_styles', get_stylesheet_uri());
 }
 
-add_action('wp_enqueue_scripts', 'dw-foundation_files');
+add_action('wp_enqueue_scripts', 'dw_foundation_files');
 
-function dw-foundation_features() {
+function dw_foundation_features() {
   add_theme_support('title-tag');
 }
 
-add_action('after_setup_theme', 'dw-foundation_features');
+add_action('after_setup_theme', 'dw_foundation_features');
 
-function dw-foundation_adjust_queries($query) {
+function dw_foundation_adjust_queries($query) {
   if (!is_admin() AND is_post_type_archive('event') AND is_main_query()) {
     $today = date('Ymd');
     $query->set('meta_key', 'event_date');
@@ -32,7 +32,7 @@ function dw-foundation_adjust_queries($query) {
   }
 }
 
-add_action('pre_get_posts', 'dw-foundation_adjust_queries');
+add_action('pre_get_posts', 'dw_foundation_adjust_queries');
 
 // Redirect subscriber accounts out of admin and onto homepage
 add_action('admin_init', 'redirectSubsToFrontend');
@@ -64,7 +64,7 @@ function ourHeaderUrl() {
 }
 
 function ourLoginCSS() {
-  wp_enqueue_style('dw-foundation_main_styles', get_stylesheet_uri());
+  wp_enqueue_style('dw_foundation_main_styles', get_stylesheet_uri());
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 }
 
@@ -74,4 +74,4 @@ function ourLoginTitle() {
   return get_bloginfo('name');
 }
 
-add_filter('login_headertitle', 'ourLoginTitle');
+add_filter('login_headertex', 'ourLoginTitle');
